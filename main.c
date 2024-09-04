1cbff8ab69c28c7081f361a661bf9c948149e088
lautipaletta <lautaropaletta1@gmail.com>
2024-09-04 20:02:12 -0300

    feat: add structs to keep code clean

--- main.c
+++ main.c
@@ -6,18 +6,27 @@
 
 #define SHM_NAME "md5_shm\0"          // este define queda aca
 
-
 void write_to_result_file(char* buffer);
 int share_to_view_process(char *buffer, const char *data);
+
+typedef struct CompletionStatus {
+    int tasks_sent;
+    int tasks_completed;
+    int tasks_completed_by_slave[SLAVES_QTY];
+} CompletionStatus;
+
+typedef struct ComunicationPipes {
+    int request[SLAVES_QTY][2];
+    int response[SLAVES_QTY][2];
+} ComunicationPipes;
 
 int main(int argc, char *argv[]) {
 
     pid_t pids[SLAVES_QTY];
-    int request_pipes[SLAVES_QTY][2];
-    int response_pipes[SLAVES_QTY][2];
-    int tasks_sent = 0;
-    int tasks_completed = 0;
     int filesQty = argc - 1;
+
+    CompletionStatus* completion_status = malloc(sizeof(CompletionStatus));
+    ComunicationPipes* comunication_pipes = malloc(sizeof(ComunicationPipes));
 
     // Create shm to share semaphores
     int sem_fd = open_shm(SHM_SEM_NAME, 
@@ -27,9 +36,9 @@ int main(int argc, char *argv[]) {
 
     create_shm_space(SHM_NAME, sem_fd, sizeof(sem_t));
 
-    sem_t *sem = map_shm(sem_fd, sizeof(sem_t), PROT_READ | PROT_WRITE);
+    sem_t *shm_sem = map_shm(sem_fd, sizeof(sem_t), PROT_READ | PROT_WRITE);
         
-    if (sem_init(sem, 1, 0) == -1)      // creamos un semaforo que se compartira y lo mapeamos a la shm
+    if (sem_init(shm_sem, 1, 0) == -1)      // creamos un semaforo que se compartira y lo mapeamos a la shm
     {
         printf("Cannot create semaphore.\n");
         return -1;
@@ -54,15 +63,15 @@ int main(int argc, char *argv[]) {
 
     shm_buffer[total_size - 1] = ASCII_EOF;
 
-    printf("%s\n", SHM_NAME);             // Compartimos el nombre de la shared memory ya creada por salida estandar
+    printf("%s",SHM_NAME);             // Compartimos el nombre de la shared memory ya creada por salida estandar
     fflush(stdout);                     // Fuerzo a imprimir
     sleep(2);
     //-------------------------------------------------------------------------------------------------
 
     // Iniciamos los procesos esclavos
     for(int i = 0; i < SLAVES_QTY; i++) {
-        pipe(request_pipes[i]);                     // Creo un pipe para enviar las peticiones de hasheo al esclavo.
-        pipe(response_pipes[i]);                    // Creo un pipe para recibir el MD5 del archivo que envié como request.
+        pipe(comunication_pipes->request[i]);                     // Creo un pipe para enviar las peticiones de hasheo al esclavo.
+        pipe(comunication_pipes->response[i]);                    // Creo un pipe para recibir el MD5 del archivo que envié como request.
         pids[i] = fork();                           // Forkeo el proceso para dividir la ejecución en dos y almaceno el PID (en el proceso padre) en el array pids.
 
         if(pids[i] == -1) {
@@ -72,12 +81,12 @@ int main(int argc, char *argv[]) {
 
         if(pids[i] == 0) {                          // Estamos en el proceso hijo.
             // Configuramos los pipes.
-            close(request_pipes[i][1]);             // Cierro el extremo de escritura del request pipe (no lo voy a usar).
-            close(response_pipes[i][0]);            // Cierro el extremo de lectura del response pipe (no lo voy a usar).
-            dup2(request_pipes[i][0], STDIN_FILENO); // Redirijo la entrada estándar al extremo de lectura del request pipe.
-            dup2(response_pipes[i][1], STDOUT_FILENO); // Redirijo la salida estándar al extremo de escritura del response pipe.
-            close(request_pipes[i][0]);              // Cierro el extremo de lectura del request pipe (ya lo redirigí).
-            close(response_pipes[i][1]);             // Cierro el extremo de escritura del response pipe (ya lo redirigí).
+            close(comunication_pipes->request[i][1]);             // Cierro el extremo de escritura del request pipe (no lo voy a usar).
+            close(comunication_pipes->response[i][0]);            // Cierro el extremo de lectura del response pipe (no lo voy a usar).
+            dup2(comunication_pipes->request[i][0], STDIN_FILENO); // Redirijo la entrada estándar al extremo de lectura del request pipe.
+            dup2(comunication_pipes->response[i][1], STDOUT_FILENO); // Redirijo la salida estándar al extremo de escritura del response pipe.
+            close(comunication_pipes->request[i][0]);              // Cierro el extremo de lectura del request pipe (ya lo redirigí).
+            close(comunication_pipes->response[i][1]);             // Cierro el extremo de escritura del response pipe (ya lo redirigí).
             int res = execve("./slave", NULL, NULL);           // Ejecuto el programa esclavo.
             if(res == -1) {
                 char errorMsg[49];
@@ -87,8 +96,8 @@ int main(int argc, char *argv[]) {
             }
         } else {                                    // Estamos en el proceso padre.
             // Configuramos los pipes.
-            close(request_pipes[i][0]);             // Cierro el extremo de lectura del request pipe (no lo voy a usar).
-            close(response_pipes[i][1]);            // Cierro el extremo de escritura del response pipe (no lo voy a usar).
+            close(comunication_pipes->request[i][0]);             // Cierro el extremo de lectura del request pipe (no lo voy a usar).
+            close(comunication_pipes->response[i][1]);            // Cierro el extremo de escritura del response pipe (no lo voy a usar).
         }
     }
 
@@ -97,13 +106,13 @@ int main(int argc, char *argv[]) {
     int continue_sending = 1;
     for (int i = 0; i < SLAVES_QTY && continue_sending; i++) {
         for(int j = 0; j < TASKS_QTY; j++) {
-            if(tasks_sent >= filesQty) {
+            if(completion_status->tasks_sent >= filesQty) {
                 continue_sending = 0;
                 break;
             }
-            char* task = argv[tasks_sent + 1];
-            write(request_pipes[i][1], task, strlen(task) + 1);
-            tasks_sent++;
+            char* task = argv[completion_status->tasks_sent + 1];
+            write(comunication_pipes->request[i][1], task, strlen(task) + 1);
+            completion_status->tasks_sent++;
         }
     }
 
@@ -112,12 +121,12 @@ int main(int argc, char *argv[]) {
 
     fd_set response_pipes_fds; // Estos son los file descriptors que vamos a monitorear para saber si hay información disponible para leer.
 
-    while(tasks_completed < filesQty) {
+    while(completion_status->tasks_completed < filesQty) {
         FD_ZERO(&response_pipes_fds);                       // Los inicializamos todos en 0 en cada iteración del while, esto es necesario dado que select() modifica el set que le pasamos para indicar qué file descriptors tienen información disponible.
         int max_fd = -1;                                    // Usamos esta variable para guardar el file descriptor más grande que vamos a monitorear, dado que select() necesita que le pasemos esto (+1) por parámetro.
 
         for (int i = 0; i < SLAVES_QTY; i++) {
-            int response_pipe_read_fd = response_pipes[i][0];
+            int response_pipe_read_fd = comunication_pipes->response[i][0];
             FD_SET(response_pipe_read_fd, &response_pipes_fds);
             if (response_pipe_read_fd > max_fd) {
                 max_fd = response_pipe_read_fd;
@@ -135,9 +144,10 @@ int main(int argc, char *argv[]) {
         // Ahora verificamos en cuál de los pipes hay información disponible para leer.
         for (int i = 0; i < SLAVES_QTY; i++) {
             // Preguntamos si en el pipe de índice i hay información disponible para leer.
-            int read_fd = response_pipes[i][0];
+            int read_fd = comunication_pipes->response[i][0];
             if (FD_ISSET(read_fd, &response_pipes_fds)) {
-                tasks_completed++;
+                completion_status->tasks_completed++;
+                completion_status->tasks_completed_by_slave[i]++;
 
                 // Hay un MD5 disponible en el pipe, lo leemos.
                 char md5_response_buffer[60];
@@ -153,33 +163,38 @@ int main(int argc, char *argv[]) {
 
                 // Agregamos el resultado al archivo resultado.txt.
                 write_to_result_file(buffer);
-                sem_post(sem);
+                sem_post(shm_sem);
 
                 // Compartimos el resultado con el proceso vista.
                 shm_buffer += share_to_view_process(shm_buffer, buffer);
 
-                // Verificamos si hay más tareas para enviar.
-                if(tasks_sent < filesQty) {
-                    char* task = argv[tasks_sent + 1];
-                    write(request_pipes[i][1], task, strlen(task) + 1);
-                    tasks_sent++;
-                } else {
-                    // No hay más tareas para enviar, cerramos el pipe de escritura para que el esclavo termine.
-                    close(request_pipes[i][1]);
+                // Verificamos si los esclavos completaron las tareas iniciales y luego si hay más tareas para enviar.
+                if(completion_status->tasks_completed_by_slave[i] >= TASKS_QTY) {
+                    if(completion_status->tasks_sent < filesQty) {
+                        char* task = argv[completion_status->tasks_sent + 1];
+                        write(comunication_pipes->request[i][1], task, strlen(task) + 1);
+                        completion_status->tasks_sent++;
+                    } else {
+                        // No hay más tareas para enviar, cerramos el pipe de escritura para que el esclavo termine.
+                        close(comunication_pipes->request[i][1]);
+                    }
                 }
             }
         }
     }
 
     *shm_buffer = ASCII_EOF;        // ponemos en el EOF para que el programa vista sepa que ya esta
-    sem_post(sem);
+    sem_post(shm_sem);
 
     munmap(shm_buffer, (sizeof(char) * total_size) + 1);
     close(fd);
 
 
-    munmap(sem, sizeof(sem_t));             // Desmapeamos el espacio de memoria del programa
+    munmap(shm_sem, sizeof(sem_t));             // Desmapeamos el espacio de memoria del programa
     close(sem_fd);                          // cerramos el file descriptor
+
+    free(completion_status);
+    free(comunication_pipes);
 
     return 0;
 
